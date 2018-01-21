a0ef470167832893b6fb6ba0ffc92932f1d5803b
Nhat Ha <nhathadt11@gmail.com>
2018-01-21 22:12:20 +0700

    Simple use of station repository and beauty printer

--- src/main/java/study/nhatha/swd/App.java
+++ src/main/java/study/nhatha/swd/App.java
@@ -1,13 +1,26 @@
 package study.nhatha.swd;
 
+import study.nhatha.swd.console.Printer;
+import study.nhatha.swd.station.StationRepository;
+import study.nhatha.swd.util.Database;
+import java.util.Objects;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+  public static void main( String[] args )
+  {
+    Objects.requireNonNull(Database.getConnection(), "Problem connecting to server");
+    System.out.println("Connection established");
+    StationRepository stationRepository = new StationRepository();
+    Printer.printWithHeaders(
+        "%-10s %-10s %s%n",
+        new String[] {"Id", "Code", "Name"},
+        stationRepository.queryAll()
+    );
+  }
 }
