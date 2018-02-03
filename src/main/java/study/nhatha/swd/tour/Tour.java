99dff5ea8c6b4b772a78bb970b0c504f2b2b70e1
Nhat Ha <nhathadt11@gmail.com>
2018-02-03 17:10:46 +0700

    Change structure of Tour and Train

--- src/main/java/study/nhatha/swd/tour/Tour.java
+++ src/main/java/study/nhatha/swd/tour/Tour.java
@@ -4,6 +4,14 @@ public class Tour {
   private int id;
   private String code;
   private String name;
+  private String stationCode;
+  private String stationName;
+  private String trackCode;
+  private String trackName;
+  private String sourceCode;
+  private String sourceName;
+  private String destinationCode;
+  private String destinationName;
 
   public Tour(String code, String name) {
     this.code = code;
@@ -14,6 +22,20 @@ public class Tour {
     this.id = id;
     this.code = code;
     this.name = name;
+  }
+
+  public Tour(int id, String code, String name, String stationCode, String stationName, String trackCode, String trackName, String sourceCode, String sourceName, String destinationCode, String destinationName) {
+    this.id = id;
+    this.code = code;
+    this.name = name;
+    this.stationCode = stationCode;
+    this.stationName = stationName;
+    this.trackCode = trackCode;
+    this.trackName = trackName;
+    this.sourceCode = sourceCode;
+    this.sourceName = sourceName;
+    this.destinationCode = destinationCode;
+    this.destinationName = destinationName;
   }
 
   public int getId() {
@@ -40,8 +62,75 @@ public class Tour {
     this.name = name;
   }
 
+  public String getStationCode() {
+    return stationCode;
+  }
+
+  public void setStationCode(String stationCode) {
+    this.stationCode = stationCode;
+  }
+
+  public String getStationName() {
+    return stationName;
+  }
+
+  public void setStationName(String stationName) {
+    this.stationName = stationName;
+  }
+
+  public String getTrackCode() {
+    return trackCode;
+  }
+
+  public void setTrackCode(String trackCode) {
+    this.trackCode = trackCode;
+  }
+
+  public String getTrackName() {
+    return trackName;
+  }
+
+  public void setTrackName(String trackName) {
+    this.trackName = trackName;
+  }
+
+  public String getSourceCode() {
+    return sourceCode;
+  }
+
+  public void setSourceCode(String sourceCode) {
+    this.sourceCode = sourceCode;
+  }
+
+  public String getSourceName() {
+    return sourceName;
+  }
+
+  public void setSourceName(String sourceName) {
+    this.sourceName = sourceName;
+  }
+
+  public String getDestinationCode() {
+    return destinationCode;
+  }
+
+  public void setDestinationCode(String destinationCode) {
+    this.destinationCode = destinationCode;
+  }
+
+  public String getDestinationName() {
+    return destinationName;
+  }
+
+  public void setDestinationName(String destinationName) {
+    this.destinationName = destinationName;
+  }
+
   @Override
   public String toString() {
-    return String.format("%5d %10s %10s", id, code, name);
+    return String.format(
+        "%5d %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s",
+        id, code, name, stationCode, stationName, trackCode, trackName, sourceCode, sourceName, destinationCode, destinationName
+    );
   }
 }
--- src/main/java/study/nhatha/swd/train/Train.java
+++ src/main/java/study/nhatha/swd/train/Train.java
@@ -4,6 +4,8 @@ public class Train {
   private int id;
   private String code;
   private String name;
+  private int seatNum;
+  private float seatPrice;
 
   public Train(String code, String name) {
     this.code = code;
@@ -14,6 +16,14 @@ public class Train {
     this.id = id;
     this.code = code;
     this.name = name;
+  }
+
+  public Train(int id, String code, String name, int seatNum, float seatPrice) {
+    this.id = id;
+    this.code = code;
+    this.name = name;
+    this.seatNum = seatNum;
+    this.seatPrice = seatPrice;
   }
 
   public int getId() {
@@ -40,8 +50,24 @@ public class Train {
     this.name = name;
   }
 
+  public int getSeatNum() {
+    return seatNum;
+  }
+
+  public void setSeatNum(int seatNum) {
+    this.seatNum = seatNum;
+  }
+
+  public float getSeatPrice() {
+    return seatPrice;
+  }
+
+  public void setSeatPrice(float seatPrice) {
+    this.seatPrice = seatPrice;
+  }
+
   @Override
   public String toString() {
-    return String.format("%5d %10s %10s", id, code, name);
+    return String.format("%5d %10s %10s %5d %10f", id, code, name, seatNum, seatPrice);
   }
 }
