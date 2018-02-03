c82353d00dfc77250c58b8ed2cdc784b46e504a7
Nhat Ha <nhathadt11@gmail.com>
2018-02-04 00:15:25 +0700

    Add Strings util and console printer
    String joining
    More powerful printer helper methods

--- src/main/java/study/nhatha/swd/console/Printer.java
+++ src/main/java/study/nhatha/swd/console/Printer.java
@@ -1,14 +1,45 @@
 package study.nhatha.swd.console;
 
-import java.util.List;
+import study.nhatha.swd.util.Args;
 
-public class Printer {
-  public static void print(String format, String[] args) {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class Printer {
+  private Printer() {
+  }
+
+  public static void inlinePretty(String format, String[] args) {
     System.out.printf(format, args);
   }
 
-  public static void printWithHeaders(String format, String[] headers, List list) {
-    print(format, headers);
+  public static void newlinePretty(String format, String[] args) {
+    System.out.printf(format + "%n", args);
+  }
+
+  public static void manyWithHeaders(String format, String[] headers, List list) {
+    newlinePretty(format, headers);
     list.forEach(System.out::println);
+  }
+
+  public static void inline(String message) {
+    System.out.print(message);
+  }
+
+  public static void inline(String delimiter, String... messages) {
+    System.out.print(joining(delimiter, messages));
+  }
+
+  public static void newline(String message) {
+    System.out.println(message);
+  }
+
+  public static void newline(String delimiter, String... messages) {
+    System.out.println(joining(delimiter, messages));
+  }
+
+  private static String joining(String delimiter, String... messages) {
+    return Stream.of(messages).collect(Collectors.joining(delimiter));
   }
 }
--- src/main/java/study/nhatha/swd/util/Strings.java
+++ src/main/java/study/nhatha/swd/util/Strings.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public final class Sql {
+public final class Strings {
   public static String makeJoin(String token, int number, String delimiter) {
     String[] strings = new String[number];
     Arrays.fill(strings, token);
 
     return Stream.of(strings).collect(Collectors.joining(delimiter));
   }
-  private Sql() {
+  private Strings() {
   }
 }
