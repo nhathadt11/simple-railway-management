66d9420603a0445a75463b7444ac127bbdfdbe44
Nhat Ha <nhathadt11@gmail.com>
2018-02-04 22:16:03 +0700

    Add request input date

--- src/main/java/study/nhatha/swd/console/Inputer.java
+++ src/main/java/study/nhatha/swd/console/Inputer.java
@@ -1,9 +1,14 @@
 package study.nhatha.swd.console;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
 import java.util.Scanner;
 
 public final class Inputer {
   private static final Scanner SCANNER = new Scanner(System.in);
+  private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yy-MM-dd HH:mm");
   private Inputer() {
   }
 
@@ -32,5 +37,22 @@ public final class Inputer {
   public static String requestString(String label) {
     Printer.inline(label);
     return requestString();
+  }
+
+  public static Date requestDate() {
+    Date date = null;
+
+    try {
+      date = new Date(DATE_FORMAT.parse(SCANNER.nextLine().trim()).getTime());
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+
+    return date;
+  }
+
+  public static Date requestDate(String label) {
+    Printer.inline(label);
+    return requestDate();
   }
 }
